a5aca433656ccaf7ef1f841ed6b26352a00c8ac2
Felipe Rotermel <feliperotermell@gmail.com>
2023-11-24 13:32:56 -0300

    Criação do History

---
+++ src/components/home/History.vue
@@ -0,0 +1,57 @@
+<script></script>
+
+<template>
+    <div class="container-fluid overflow-x-hidden">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h3 class="main-title display-6 fw-bold">Histórico dos Hackathons:</h3>
+                    <hr class="opacity-100">
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2023</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2022</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2021</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2020</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2019</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2018</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2017</button>
+                </div>
+                <div class="col">
+                  <button type="button" class="btn btn-outline-info">2016</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.container-fluid {
+    position: relative;
+    padding-bottom: 50px;
+    background-image: url("@/assets/img/Pattern1.jpg");
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.col-12 {
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
   import NavBar from "@/components/nav/NavBar.vue";
   import BGintro from "@/components/home/BGintro.vue";
+  import History from "@/components/home/History.vue";
 </script>
 
 <template>
   <NavBar />
   <BGintro />
+  <History />
 </template>
