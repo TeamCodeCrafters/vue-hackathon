8cf47757ebed78b0ba836fe499fede598087b683
Felipe Rotermel <feliperotermell@gmail.com>
2023-11-24 12:29:49 -0300

    Criação da BGintro

---
+++ src/components/home/BGintro.vue
@@ -0,0 +1,58 @@
+<script></script>
+
+<template>
+    <div class="container-fluid overflow-x-hidden">
+        <div class="carousel slide w-100">
+            <div class="carousel-inner"></div>
+            <div class="carousel-caption d-flex align-items-center justify-content-center text-center">
+                <div class="text">
+                    <h1><strong>Hackathon</strong></h1>
+                    <h5>Site administrativo para equipes do Hackathon</h5>
+                    <button type="button" class="btn btn-outline-light fs-4" v-scroll-to="'#aboutHackathon'">
+                        Introdução
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.container-fluid {
+    background-color: rgb(232, 232, 232);
+    padding: 0;
+    margin: 0;
+    background-image: url("@/assets/img/BGintro.png");
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.carousel {
+    min-height: 500px;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+}
+
+.carousel-caption {
+    margin-bottom: 20vh;
+}
+
+.text {
+    margin-bottom: 7%;
+}
+
+h1, h5 {
+    margin-bottom: 30px;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
   import NavBar from "@/components/nav/NavBar.vue";
+  import BGintro from "@/components/home/BGintro.vue";
 </script>
 
 <template>
   <NavBar />
+  <BGintro />
 </template>
